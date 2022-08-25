6f39f9236b7d9b17b926f5d4e110f43aca4d2b2c
Risal <risalhidayat88@gmail.com>
2022-08-25 15:00:32 +0700

    removing unused require

--- lib/bitbucket-api.rb
+++ lib/bitbucket-api.rb
@@ -4,11 +4,6 @@ require 'bitbucket-api/config'
 
 # require errors
 # require 'bitbucket-api/errors'
-# require 'bitbucket-api/errors/ovo'
-# require 'bitbucket-api/errors/disbursement'
-# require 'bitbucket-api/errors/credit_card'
-# require 'bitbucket-api/errors/virtual_account'
-# require 'bitbucket-api/errors/v1/ewallet'
 
 module BitbucketApi
   extend Config
